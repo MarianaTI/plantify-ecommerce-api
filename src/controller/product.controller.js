f6be9640159ce59578423d995c68f1b8f98f0b61
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2024-10-13 19:50:14 -0600

    feat(): delete and update

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -77,7 +77,10 @@ export const getAll = async (req, res) => {
 export const getById = async (req, res) => {
   try {
     const { id } = req.params;
-    const product = await Product.findById(id).populate("id_category", "_id name");
+    const product = await Product.findById(id).populate(
+      "id_category",
+      "_id name"
+    );
 
     if (!product) {
       return res.status(404).json({ message: "Product not found" });
@@ -102,3 +105,90 @@ export const getById = async (req, res) => {
     return res.status(500).json({ message: "Error al obtener el producto" });
   }
 };
+
+export const deleteOne = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const existingProduct = await Product.findById(id);
+    if (!existingProduct) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+
+    if (existingProduct.image?.publicId) {
+      await deleteImage(existingProduct.image.publicId);
+    }
+
+    if (existingProduct.imageBackground?.publicId) {
+      await deleteImage(existingProduct.imageBackground.publicId);
+    }
+
+    await Product.findByIdAndDelete(id);
+
+    return res.status(200).json({ message: "Producto eliminado con éxito" });
+  } catch (error) {
+    console.error("Error al eliminar el producto:", error);
+    return res.status(500).json({ message: "Error al eliminar el producto", error: error.message });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id)
+    const { name, description, price, stars, id_category } = req.body;
+
+    const updateData = {};
+
+    if (name) updateData.name = name;
+    if (description) updateData.description = description;
+    if (price) updateData.price = price;
+    if (stars) updateData.stars = stars;
+    if (id_category) updateData.id_category = id_category;
+
+    const existingProduct = await Product.findById(id);
+
+    if (!existingProduct) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+
+    if (req.files?.image) {
+      if (existingProduct.image?.publicId) {
+        await deleteImage(existingProduct.image.publicId);  
+      }
+      
+      const result = await uploadImage(req.files.image.tempFilePath);
+      updateData.image = {
+        publicId: result.public_id,
+        secureUrl: result.secure_url,
+      };
+
+      await fs.unlink(req.files.image.tempFilePath);  
+    }
+
+    if (req.files?.imageBackground) {
+      if (existingProduct.imageBackground?.publicId) {
+        await deleteImage(existingProduct.imageBackground.publicId);  
+      }
+
+      const resultBackground = await uploadImage(req.files.imageBackground.tempFilePath);
+      updateData.imageBackground = {
+        publicId: resultBackground.public_id,
+        secureUrl: resultBackground.secure_url,
+      };
+
+      await fs.unlink(req.files.imageBackground.tempFilePath);  
+    }
+
+    Object.assign(existingProduct, updateData);
+
+    const update = await existingProduct.save();
+
+    return res.status(200).json({ message: "Producto actualizado con éxito", product: update });
+  } catch (error) {
+    console.error("Error al actualizar el producto:", error);
+    return res.status(500).json({ message: "Error al actualizar el producto", error: error.message });
+  }
+};
+
+
--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -5,11 +5,17 @@ import fileUpload from "express-fileupload";
 const router = Router();
 
 router.post(
-  "/create/product",
+  "/product/create",
   fileUpload({ useTempFiles: true, tempFileDir: "/uploads" }),
   productController.create
 );
 router.get("/products", productController.getAll);
 router.get("/product/:id", productController.getById);
+router.put(
+  "/product/update/:id",
+  fileUpload({ useTempFiles: true, tempFileDir: "/uploads" }),
+  productController.update
+);
+router.delete("/product/delete/:id", productController.deleteOne);
 
 export default router;
